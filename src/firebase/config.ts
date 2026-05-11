32ad30596b44427194ff6a4fc2a4ab5bc4c0b63a
Egbase John Mary <johnmaryegbase@gmail.com>
2026-05-11 09:46:56 +0000

    change the firestore database

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCn0pLLYEpAHR6ehj6zWxVnoEzQgLCFVCs",
+  authDomain: "basechanteam.firebaseapp.com",
+  projectId: "basechanteam",
+  storageBucket: "basechanteam.firebasestorage.app",
+  messagingSenderId: "261796318440",
+  appId: "1:261796318440:web:e62d9bda06dac94b264d5d",
+  measurementId: "G-WLTB42P50C"
 };
